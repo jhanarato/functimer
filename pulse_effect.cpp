96d1be31a2177d8eaf482df46c727cecbf65f402
Jhanarato Bhikkhu <jhanarato@gmail.com>
2016-02-13 12:52:40 +0800

    PulseEffect now works. That is pretty much it for the effects hierachy.

---
+++ pulse_effect.cpp
@@ -0,0 +1,42 @@
+/* 
+Copyright 2016 Bhikkhu Jhanarato 
+  
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "pulse_effect.h"
+
+PulseEffect::PulseEffect(SwitchPanel* switches, IndicatorPanel* indicators) 
+                         : LedEffect(switches, indicators)
+{
+    delayTimer.setDuration(30);
+}
+
+void PulseEffect::start()
+{
+    delayTimer.start();
+}
+
+void PulseEffect::update()
+{
+    delayTimer.update();
+    
+    if(delayTimer.isComplete())
+    {
+        indicators->allDim(fader.getLevel());
+        fader.fadeInAndOut();
+        delayTimer.start();
+    }
+}
+
+
