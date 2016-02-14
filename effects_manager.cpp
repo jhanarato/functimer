37387c0d7e20a56892585f9e1e35fc9ba635f26c
Jhanarato Bhikkhu <jhanarato@gmail.com>
2016-02-14 12:16:46 +0800

    EffectsManager implemented

---
+++ effects_manager.cpp
@@ -0,0 +1,57 @@
+/* 
+Copyright 2016 Bhikkhu Jhanarato 
+  
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "effects_manager.h"
+
+EffectsManager::EffectsManager(SwitchPanel* switches, 
+                               IndicatorPanel* indicators) 
+{
+    this->switches = switches;
+    this-> indicators = indicators;
+    
+    pulseEffect = new PulseEffect(switches, indicators);
+    blinkEffect = new BlinkEffect(switches, indicators);
+    switchIndicatorEffect = new SwitchIndicatorEffect(switches, indicators);
+    
+    // Default effect
+    currentEffect = switchIndicatorEffect;
+}
+
+void EffectsManager::start()
+{
+    currentEffect->start();
+}
+
+void EffectsManager::update()
+{
+    currentEffect->update();
+}
+
+void EffectsManager::pulse()
+{
+    currentEffect = pulseEffect;
+}
+
+void EffectsManager::blink()
+{
+    currentEffect = blinkEffect;
+}
+
+void EffectsManager::switchIndicator()
+{
+    currentEffect = switchIndicatorEffect;
+}
---
+++ effects_manager.h
@@ -0,0 +1,46 @@
+/* 
+Copyright 2016 Bhikkhu Jhanarato 
+  
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef EFFECTS_MANAGER_H
+#define EFFECTS_MANAGER_H
+
+#include "led_effect.h"
+#include "pulse_effect.h"
+#include "blink_effect.h"
+#include "switch_indicator_effect.h"
+
+class EffectsManager 
+{
+    SwitchPanel* switches;
+    IndicatorPanel* indicators;
+    
+    LedEffect* currentEffect;
+    PulseEffect* pulseEffect;
+    BlinkEffect* blinkEffect;
+    SwitchIndicatorEffect* switchIndicatorEffect;
+    
+public:
+    EffectsManager(SwitchPanel* switches, 
+                   IndicatorPanel* indicators); 
+                   
+    void start();
+    void update();
+    void pulse();
+    void blink();
+    void switchIndicator();               
+};
+
+#endif
